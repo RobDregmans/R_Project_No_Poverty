11c64d835e459b31f4b469aca9af74ea77536d5a
itjallingii <44028101+itjallingii@users.noreply.github.com>
2018-11-03 17:22:01 +0100

    gewerkt aan betere data krijgen

--- Final graphs/Barchart omschrijven.R
+++ Final graphs/Barchart omschrijven.R
@@ -26,9 +26,13 @@ layout = go.Layout(
 fig = dict(data=data, layout=layout)
 py.iplot(fig, filename='low-extreme-vs-high-extreme')
 
+columns = list('RegionCode','CountryCode', 'CountryName','RequestYear',"HeadCount",'ReqYearPopulation','PPP')
 
 
-
+df_poverty_extreme = subset(our_indicator_data,our_indicator_data$'Indicator Code' == "SI.POV.DDAY")
+df_poverty_extreme = subset(df_poverty_extreme, df_poverty_extreme$`Country Code` == 'WLD')
+df_poverty_1.90_povnet = read.table("data/CountryTable_1.9.csv", header = TRUE, sep = ',')
+df_poverty_1.90_povnet = df_poverty_1.90_povnet %>% select(columns)
 x <- c('Product A', 'Product B', 'Product C')
 y <- c(20, 14, 23)
 text <- c('27% market share', '24% market share', '19% market share')
