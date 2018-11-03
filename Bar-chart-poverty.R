5dcb18fcaf1d068d9a97dc95a7e4471328d7a92a
RobDregmans <43481942+RobDregmans@users.noreply.github.com>
2018-11-03 21:42:19 +0100

    line-chart

--- Bar-chart-poverty.R
+++ Bar-chart-poverty.R
@@ -3,18 +3,41 @@
 #Rural population (% of total population) SP.RUR.TOTL.ZS
 #Rural poverty headcount ratio at national poverty lines (% of rural population) SI.POV.RUHC
 
+WDI = read.table("WDIData.csv",sep=",",fileEncoding="UTF-8-BOM",header=TRUE)  
+new_data = read.table("CountryTable_1.9.csv",sep=",",fileEncoding="UTF-8-BOM",header=TRUE) 
+WDI = WDI[,1:62]
+colnames(WDI) <- sub("X", "", colnames(WDI))
+WDI <- WDI[,c(2:3,4:4,50:60)]
+all_var = unique(WDI[,2:3])
+pop = c("SP.POP.TOTL")
+WDI_pop = subset(WDI,WDI$Indicator.Code %in% pop)
+merged = merge(WDI,new_data,id = "Country.Code")
+
 var_rural_urban = c("SP.URB.TOTL.IN.ZS", "SI.POV.URHC", "SP.RUR.TOTL.ZS", "SI.POV.RUHC")
+newvar = c("SI.POV.DDAY", "SP.POP.TOTL")
+world = c("WLD")
 
 library(reshape)
 library(reshape2)
+library(zoo)
+library(plotly)
 
 # subset the data - selected var with all years
 
-indicator_data = subset(mydata,mydata$Indicator.Code %in% var_rural_urban)
+bar_data = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% newvar)
+bar_data = subset(bar_data,bar_data$'Country.Code' %in% world)
 
-# subset the data - selected var with only 1990 - 2015 & prepare data for plotting
-indicator_data_1990_2015 = indicator_data[,c(2:3,4:4,35:60)]
-colnames(indicator_data_1990_2015) <- sub("X", "", colnames(indicator_data_1990_2015))
+wide = dcast(bar_data, Indicator.Name + Country.Code + variable ~ Indicator.Code,  value.var = "value" , fun.aggregate = mean, na.rm = T)
 
-temp = dcast(bar_data, `Indicator Name` + `Country Code` + Region + `Region/Country Name` ~ `Indicator Code`, value.var = "Value", fun.aggregate = mean, nar.rm = T)
+wide$SP.POP.TOTL <- na.locf(wide$SP.POP.TOTL)
+wide <- na.omit(wide)
+wide$SI.POV.DDAY <- wide$SI.POV.DDAY/100
+wide$absolute <- wide$SI.POV.DDAY * wide$SP.POP.TOTL
+
+p <- plot_ly(wide, x = wide$variable, y = ~wide$absolute, type = 'scatter', mode = 'lines')
+p
+
+
+
+
 
