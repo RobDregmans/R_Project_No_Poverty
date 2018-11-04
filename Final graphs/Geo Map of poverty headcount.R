806f81b240bb8fcec242579800ce6dfd2d7c9e54
itjallingii <44028101+itjallingii@users.noreply.github.com>
2018-11-04 16:00:09 +0100

    geomap done

--- Final graphs/Geo Map of poverty headcount.R
+++ Final graphs/Geo Map of poverty headcount.R
@@ -1,28 +1,33 @@
+#selecting data from wdi + average of 6 years 2010 - 2015
+colnames(our_indicator_data) = c("Country.Code","Region","Country.Name","Indicator.Code","Indicator.Name","Year", "value")
+var_i = c("SI.POV.DDAY","SP.POP.TOTL")
+#subset the data with only the world and the two indicators above about population and people in poverty in %
+graph_data = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% var_i)
+graph_data = subset(graph_data,graph_data$'Year' %in% year)
 
-#https://plot.ly/r/choropleth-maps/ for more information
-dp= subset(our_indicator_data, our_indicator_data$'Indicator Code' == 'SI.POV.DDAY', select = c("Country Code","Region","Year","Value","Region/Country Name"))
-#due to missing data, calculate average over 6 year period
-year = list('2010','2011','2012','2013','2014','2015')
-dp = subset(dp, dp$'Year' %in% year, select = c("Country Code", "Region","Value","Region/Country Name"))
-colnames(dp) = c("code","region","value","country")
+test <- subset(graph_data, select = c("Indicator.Code","Country.Name", "Country.Code", "Year", "value"))
+df_mean_pov = dcast(test,Country.Code + Country.Name + Indicator.Code ~ Year, value.var ="value")
+df_mean_pov['mean'] = rowMeans(df_mean_pov[,4:9],na.rm = T)
+df_mean_pov = dcast(df_mean_pov,Country.Code + Country.Name  ~ Indicator.Code, value.var ="mean")
+colnames(df_mean_pov) = c("Country.Code","Country.Name","MeanHeadCount","MeanPop")
+df_mean_pov$MeanPop = df_mean_pov$MeanPop/1000000
+df_mean_pov$MeanHeadCount = df_mean_pov$MeanHeadCount/100
+df_mean_pov['MeanPovPop'] = df_mean_pov[,3]*df_mean_pov[,4]
+df_mean_pov$MeanHeadCount = df_mean_pov$MeanHeadCount*100
+df_mean_pov$MeanPop = round(df_mean_pov$MeanPop,2)
+df_mean_pov$MeanHeadCount = round(df_mean_pov$MeanHeadCount,2)
+df_mean_pov$MeanPovPop = round(df_mean_pov$MeanPovPop,2)
 
-country_names_wdi = country_wdi[1]
-colnames(country_names_wdi) = c('code')
-                 
-dp_sum = by(dp$value, dp$code, sum)
-colnames(dp_sum) = c('value')
-transform(dp_sum, average = dp_sum$value / 6)
-dp_sum = do.call(rbind,as.list(dp_sum))
-
-merge(dp_sum,country_names_wdi)
 
 # light grey boundaries
 l <- list(color = toRGB("grey"), width = 0.5)
 
-x2 <- by(x$Frequency, x$Category, sum)
-do.call(rbind,as.list(x2))
-
-dp$hover <- with(dp, paste(country, '<br>', "Region", region))
+df_mean_pov$hover <- with(df_mean_pov, paste
+                                     ( "Year: 2015",'<br>',
+                                       "Percentage under extreme poverty line:",MeanHeadCount,"%" ,'<br>',
+                                       "Mean population living in extreme poverty:",MeanPovPop,"M",'<br>',
+                                       "Total population:",MeanPop,"M",'<br>', 
+                                       "Country:", Country.Name))
 
 # specify map projection/options
 g <- list(
@@ -30,15 +35,18 @@ g <- list(
   showcoastlines = T,
   countrycolor = toRGB("grey"),
   coastlinecolor = toRGB("grey"),
-  projection = list(type = 'Mercator')
+  projection = list(type = 'Mercator') 
 )
 
-
-k <- plot_geo(dp) %>%
-  add_trace(z = ~value, text = ~hover, color = ~value, colors = 'Reds', text = ~value, locations = ~code, marker = list(line = l,color = 'rgb(255,255,255)')) %>%
-  colorbar(title = '% population below extreme poverty') %>%
+k <- plot_geo(df_mean_pov) %>%
+  add_trace(z = ~MeanHeadCount,text = ~hover, color = ~MeanHeadCount, colors= 'Reds', 
+            locations = ~Country.Code, 
+            marker = list(line = l,color = 'rgb(255,255,255)')) %>%
+  
+  colorbar(title = 'Percentage of total population living extreme poverty') %>%
   layout(
-    title = 'Prevalance of Extreme Poverty (below 1.90$ a day)',
+    title = 'Prevalance of Extreme Poverty during 2010 - 2015 (below 1.90$ a day)',
     geo = g )
 k
+
 
