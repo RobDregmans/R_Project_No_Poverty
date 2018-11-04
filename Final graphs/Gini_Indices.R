828781e2446a7f545c467e6189774518d33cb7a2
RobDregmans <43481942+RobDregmans@users.noreply.github.com>
2018-11-04 16:34:39 +0100

    plotted gini for ivars countries

--- Final graphs/Gini_Indices.R
+++ Final graphs/Gini_Indices.R
@@ -3,6 +3,8 @@ library(reshape2)
 library(ineq)
 library(plotly)
 
+cl <- c("MDG","BDI","MWI","GNB","MOZ","ZMB","LSO")
+
 # Read the data in correctly
 mydata = read.table("WDIData.csv",sep=",",fileEncoding="UTF-8-BOM",header=TRUE)  
 
@@ -20,12 +22,8 @@ colnames(id) <- sub("X", "", colnames(id))
 idm = melt(id, id=c("Indicator.Code","Indicator.Name","Country.Code", "Country.Name"), value.name = "Year")
 # throw away the rows with missing data
 idm <- na.omit(idm, cols="value")
-# get the countries with the 10 highest gini indexes. Some countries will be double, but that is not a problem
-idm2 <- idm[order(idm$value,decreasing=T)[1:10],]
-# make a vector of the countries above
-v <- idm2[,3]
 # subset with only those countries
-idm3 <- subset(idm,idm$Country.Code %in% v)
+idm3 <- subset(idm,idm$Country.Code %in% cl)
 # plot the gini idexes of those countries over time
 x <- list( title = "Years" )
 y <- list( title = "Gini Index" )
@@ -38,22 +36,33 @@ pline
 # Get the indicators for the lorenz curve
 my_var = c( "SI.DST.05TH.20","SI.DST.02ND.20","SI.DST.03RD.20", "SI.DST.04TH.20", "SI.DST.FRST.20")
 # fill in country code in V1 and run code below
-V1 = c("BOL")
+V1 = c("LSO")
 # subset the data with only the country and the needed indicators
 country_data = subset(mydata,mydata$Country.Code %in% V1)
 COUNTRY_LC_V1 = subset(country_data,country_data$Indicator.Code %in% my_var)
 # slice the needed data for the desirable year
-COUNTRY_2015 = COUNTRY_LC_V1[,c(1:4,60:60)]
+COUNTRY_2015 = COUNTRY_LC_V1[,c(1:4,55:55)]
 # create a vector for the lorenz curve function
 p20 = c( 20, 40, 60 , 80, 100)
-# dort the values of the indicators
-d20 = sort(COUNTRY_2015$X2015)
+# dort the values of the indicators. Make sure the year is the right one
+d20 = sort(COUNTRY_2015$X2010)
 # plot the lorenz curve
-Lc.ARG <- Lc(d20, p20)
-plot(Lc.ARG)
+Lc.c <- Lc(d20, p20)
+plot(Lc.c)
+
+
 gini_index_country_2015 = Gini(d20)
 
 
 
 
 
+
+
+
+
+
+# get the countries with the 10 highest gini indexes. Some countries will be double, but that is not a problem
+idm2 <- idm[order(idm$value,decreasing=T)[1:10],]
+# make a vector of the countries above
+v <- idm2[,3]
