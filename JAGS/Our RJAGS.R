4ca4f6c76d72e86d7b2405c5466172f0bc8ff123
martijnvanvlie <martijnvanvliet95@hotmail.com>
2018-11-07 00:53:49 +0100

    poverty is taken by the mean

--- JAGS/Our RJAGS.R
+++ JAGS/Our RJAGS.R
@@ -1,21 +1,36 @@
 #selecting data from wdi
-var_jags = c("SI.POV.DDAY","SP.DYN.LE00.IN","SN.ITK.DEFC.ZS","SH.DYN.MORT")
+var_jags = c("SP.DYN.LE00.IN","SN.ITK.DEFC.ZS","SH.DYN.MORT")
 
 #subset the data with only the world and the two indicators above about population and people in poverty in %
 jags_data = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% var_jags)
 jags_data = subset(jags_data, select = c("Indicator.Code","Region","Country.Name", "Country.Code", "Year", "Value"))
 jags_data = dcast(jags_data,Country.Code + Region + Country.Name + Year ~ Indicator.Code, value.var ="Value")
-df_jags_year = subset(jags_data, jags_data$Year==2012)
+df_jags_year = subset(jags_data, jags_data$Year==2015)
 df_jags_year = df_jags_year[-which(rowSums(is.na(df_jags_year))> 0),]
+df_jags_year = subset(df_jags_year, df_jags_year$Country.Code %in% all_countries$Country.Code)
 
-#order to omit regions, only hold countries
-df_jags_yearNA = df_jags_year[order(df_jags_year$Region,df_jags_year$Country.Name),]
-x = as.numeric(dim(df_jags_yearNA))
-k = x[1]
-df_jags_yearNA = df_jags_yearNA[c(13:k),]
+### is the script Average poverty 2010 - 2015
+#selecting data from wdi + average of 6 years 2010 - 2015 -----------
+year = c('2010','2011', '2012','2013','2014','2015')
+R_jags_average = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% "SI.POV.DDAY")
+R_jags_average = subset(R_jags_average,R_jags_average$Year %in% year)
+R_jags_average = subset(R_jags_average, select = c("Indicator.Code","Country.Name", "Country.Code", "Year", "Value"))
 
+#Calculate average over columns
+R_jags_average = dcast(R_jags_average,Country.Code + Country.Name + Indicator.Code ~ Year, value.var ="Value")
+R_jags_average['Mean'] = rowMeans(R_jags_average[,4:9],na.rm = T)
+R_jags_average = R_jags_average[,c(1:2,10:10)]
 
-poverty = df_jags_year$SI.POV.DDAY
+#dropping zero's and NA's
+R_jags_average = na.omit(R_jags_average)
+R_jags_average = R_jags_average[apply(R_jags_average!=0, 1, all),]
+### ------------------------------------------------------------------
+
+df_mean = subset(R_jags_average,R_jags_average$Country.Code %in% df_jags_year$Country.Code)
+df_jags_year2 = subset(df_jags_year,df_jags_year$Country.Code %in% df_mean$Country.Code)
+df_jags_year = cbind(df_jags_year2,df_mean$Mean)
+
+poverty = df_jags_year$`df_mean$Mean`
 life_expacteny = df_jags_year$SP.DYN.LE00.IN
 undernourishment = df_jags_year$SN.ITK.DEFC.ZS
 child_mortality = df_jags_year$SH.DYN.MORT
@@ -83,8 +98,6 @@ traceplot(samp)
 # sometimes the gelman plot won't fit on a screen
 # we havve to reduce the margins
 par(mar=c(.4,.4,.4,.4))
-gelman.plot(samp)
-gelman.diag(samp)
 densplot(samp)
 acfplot(samp)
 
