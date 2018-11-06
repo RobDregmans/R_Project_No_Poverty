e8ae9ecf40b6b91272d8d7fcd4d13bf3e50f5215
itjallingii <44028101+itjallingii@users.noreply.github.com>
2018-11-06 21:49:56 +0100

    R markdown updated and correlation charts + table added

--- correlation script.R
+++ correlation script.R
@@ -1,14 +1,26 @@
-cor(x, y,  method = "pearson", use = "complete.obs")
-
 var_correlation = c("SI.POV.DDAY","SP.DYN.LE00.IN","SN.ITK.DEFC.ZS","SH.DYN.MORT")
-colnames(our_indicator_data) = c("Country.Code","Region","Country.Name","Indicator.Code","Indicator.Name","Year", "value")
 
 #subset the data with only the world and the two indicators above about population and people in poverty in %
-jags_data = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% var_correlation)
-jags_data = subset(jags_data, select = c("Indicator.Code","Country.Name", "Country.Code", "Year", "value"))
-df_correlation = dcast(jags_data,Country.Code + Country.Name + Year ~ Indicator.Code, value.var ="value")
+df_correlation = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% var_correlation)
+df_correlation = subset(df_correlation, select = c("Indicator.Code", "Country.Code", "Year", "Value"))
+df_correlation = dcast(df_correlation,Country.Code + Year ~ Indicator.Code, value.var ="Value")
 df_correlation = subset(df_correlation, df_correlation$Year==2012)
+df_correlation = na.omit(df_correlation)
+df_correlation = df_correlation[,c(3:6)]
 
 i = df_correlation$"SP.DYN.IMRT.IN"
 
-cor(df_correlation$SI.POV.DDAY,i ,method = "pearson", use = "complete.obs")
+cor_table <- cor(df_correlation, method = "pearson")
+cor_table = round(cor_table,2)
+
+install.packages("PerformanceAnalytics")
+
+library("PerformanceAnalytics")
+
+chart.Correlation(df_correlation, histogram=TRUE, pch=19)
+
+
+#In the  plot, the distribution of each variable is shown on the diagonal.
+#On the bottom of the diagonal : the bivariate scatter plots with a fitted line are displayed
+#On the top of the diagonal : the value of the correlation plus the significance level as stars
+#Each significance level is associated to a symbol : p-values(0, 0.001, 0.01, 0.05, 0.1, 1) <=> symbols(“***”, “**”, “*”, “.”, " “)
