877c2e6d2e9a11f237d959d55b22e1d931ffef65
itjallingii <44028101+itjallingii@users.noreply.github.com>
2018-11-07 21:07:58 +0100

    added some jags models with normal and 2 distributions

--- JAGS/Fitting distributions.R
+++ JAGS/Fitting distributions.R
@@ -1,26 +1,25 @@
 library(MASS)
 library(fitdistrplus)
 
-
-
-fit1 <- fitdistr(df_jags_year$`df_mean$Mean`, densfun="exponential")  # we assume my_data ~ Normal(?,?)
-fit2 <- fitdistr(df_jags_year$SH.DYN.MORT`, densfun="exponential")  # we assume my_data ~ Normal(?,?)
+#fit paraeters with custom distributions
+fit1 <- fitdistr(df_jags_year$`df_mean$Mean`, densfun="Weibull")  # we assume my_data ~ Normal(?,?)
 fit3 <- fitdistr(df_jags_year$SP.DYN.LE00.IN, densfun="Weibull")  # we assume my_data ~ Normal(?,?)
 fit4 <- fitdistr(df_jags_year$SN.ITK.DEFC.ZS, densfun="exponential")  # we assume my_data ~ Normal(?,?)
-
 fit3 <- fitdistr(df_jags_year$SH.DYN.MORT, densfun="exponential")  # we assume my_data ~ Normal(?,?)
 
-hist(df_jags_year$SN.ITK.DEFC.ZS, pch=20, breaks=15, prob=TRUE, main="")
-curve(dexp(x,0.082930,log = FALSE), col="red", lwd=2, add=
+#producing a histogram to check distributions
+hist(df_jags_year$`df_mean$Mean`, pch=20, breaks=15, prob=TRUE, main="")
 
-
+#plotting curves in histogram to check distribution with actual values
+curve(dexp(x,0.082930,log = FALSE), col="red", lwd=2, add=T)
+curve(dweibull(x, 0.5426,8.36158, log = FALSE), col="red", lwd=2, add=T)
 curve(dexp(x, fit3$estimate[1],log = FALSE), col="red", lwd=2, add=T)
 curve(dexp(x, 0.07209, log = FALSE), col="red", lwd=2, add=T)
-
-
-
 curve(dweibull(x, 14, 74.74,log = FALSE), col="red", lwd=2, add=T)
 
 
-dno
-
+#fit normal distributions for the RJAGS - normal distribution model
+fit1 <- fitdistr(df_jags_year$`df_mean$Mean`, densfun="normal")  # we assume my_data ~ Normal(?,?)
+fit2 <- fitdistr(df_jags_year$SP.DYN.LE00.IN, densfun="normal")  # we assume my_data ~ Normal(?,?)
+fit3 <- fitdistr(df_jags_year$SN.ITK.DEFC.ZS, densfun="normal")  # we assume my_data ~ Normal(?,?)
+fit4 <- fitdistr(df_jags_year$SH.DYN.MORT, densfun="normal")  # we assume my_data ~ Normal(?,?)
---
+++ JAGS/Our RJAGS-normalbeta's good.R
@@ -0,0 +1,124 @@
+#selecting data from wdi
+var_jags = c("SP.DYN.LE00.IN","SN.ITK.DEFC.ZS","SH.DYN.MORT")
+
+#subset the data with only the world and the two indicators above about population and people in poverty in %
+jags_data = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% var_jags)
+jags_data = subset(jags_data, select = c("Indicator.Code","Region","Country.Name", "Country.Code", "Year", "Value"))
+jags_data = dcast(jags_data,Country.Code + Region + Country.Name + Year ~ Indicator.Code, value.var ="Value")
+df_jags_year = subset(jags_data, jags_data$Year==2015)
+df_jags_year = df_jags_year[-which(rowSums(is.na(df_jags_year))> 0),]
+df_jags_year = subset(df_jags_year, df_jags_year$Country.Code %in% all_countries$Country.Code)
+
+### is the script Average poverty 2010 - 2015
+#selecting data from wdi + average of 6 years 2010 - 2015 -----------
+year = c('2010','2011', '2012','2013','2014','2015')
+R_jags_average = subset(our_indicator_data,our_indicator_data$'Indicator.Code' %in% "SI.POV.DDAY")
+R_jags_average = subset(R_jags_average,R_jags_average$Year %in% year)
+R_jags_average = subset(R_jags_average, select = c("Indicator.Code","Country.Name", "Country.Code", "Year", "Value"))
+
+#Calculate average over columns
+R_jags_average = dcast(R_jags_average,Country.Code + Country.Name + Indicator.Code ~ Year, value.var ="Value")
+R_jags_average['Mean'] = rowMeans(R_jags_average[,4:9],na.rm = T)
+R_jags_average = R_jags_average[,c(1:2,10:10)]
+
+#dropping zero's and NA's
+R_jags_average = na.omit(R_jags_average)
+R_jags_average = R_jags_average[apply(R_jags_average!=0, 1, all),]
+### ------------------------------------------------------------------
+
+df_mean = subset(R_jags_average,R_jags_average$Country.Code %in% df_jags_year$Country.Code)
+df_jags_year2 = subset(df_jags_year,df_jags_year$Country.Code %in% df_mean$Country.Code)
+df_jags_year = cbind(df_jags_year2,df_mean$Mean)
+
+poverty = df_jags_year$`df_mean$Mean`
+life_expacteny = df_jags_year$SP.DYN.LE00.IN
+undernourishment = df_jags_year$SN.ITK.DEFC.ZS
+child_mortality = df_jags_year$SH.DYN.MORT
+n = nrow(df_jags_year)
+
+x <- poverty
+y1 <- life_expacteny
+y2 <- undernourishment
+y3 <- child_mortality
+
+library(rjags)
+
+x <- (x-mean(x))/sd(x)
+y1 <- (y1-mean(y1))/sd(y1)
+y2 <- (y2-mean(y2))/sd(y2)
+y3 <- (y3-mean(y3))/sd(y3)
+
+model_string <- "model{
+
+# Likelihood
+for(i in 1:n){
+x[i]   ~ dnorm(mu[i],inv.var1)
+mu[i] <- beta1 + beta2*y1[i] + beta3*y2[i] + beta4*y3[i]
+}
+
+# Prior for beta
+beta1 ~ dnorm(13.871174,19.331200)
+beta2 ~ dnorm(71.3757905,7.8022477)
+beta3 ~ dnorm(12.0573913,11.6544603)
+beta4 ~ dnorm(32.040000,30.588289)
+
+
+# Prior for the inverse variance
+var1   ~ dunif(0.01, 0.99)
+var2   ~ dunif(0.01, 0.99)
+var3   ~ dunif(0.01, 0.99)
+var4   ~ dunif(0.01, 0.99)
+var5   ~ dunif(0.01, 0.99)
+inv.var1   <- 1/var1
+inv.var2   <- 1/var2
+inv.var3   <- 1/var3
+inv.var4   <- 1/var4
+inv.var5   <- 1/var5
+sigma1     <- sqrt(var1)
+sigma2     <- sqrt(var2)
+sigma3     <- sqrt(var3)
+sigma4     <- sqrt(var4)
+sigma5     <- sqrt(var5)
+}"
+
+model <- jags.model(textConnection(model_string), data = list(x=x, y1=y1,y2=y2,y3=y3,n=n))
+
+update(model, 10000, progress.bar="none"); # Burn-in for 10000 samples
+
+samp <- coda.samples(model, 
+                     variable.names=c("beta1","beta2","beta3","beta4","sigma1","sigma2","sigma3","sigma4","sigma5" ), 
+                     n.iter=20000, progress.bar="text")
+
+summary(samp)
+
+saveRDS(samp,"modelrun.rds")
+
+traceplot(samp)
+
+# sometimes the gelman plot won't fit on a screen
+# we havve to reduce the margins
+par(mar=c(.4,.4,.4,.4))
+densplot(samp)
+acfplot(samp)
+
+# get the effective sample size
+effectiveSize(samp)
+
+sampmatrix = as.matrix(samp)
+
+# take the first four parameters, but drop off the noise!
+param <-sampmatrix[sample(nrow(sampmatrix),size = 100, replace=FALSE),1:4]
+
+# create a design matrix
+n <- nrow(df_jags_year)
+# make an intercept
+intercept <- rep(1,n)
+# bind your data together in the order used for modelling
+data <- cbind(intercept,y1,y2,y3)
+pred <- param %*% t(data)
+pred <- t(pred)
+# replicate the results
+act <- rep(x,100)
+par(mar=c(1,1,1,1))
+dev.off()
+plot(act,pred,xlab="Actual",ylab="Predicted")
